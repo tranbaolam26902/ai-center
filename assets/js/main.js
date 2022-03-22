7bceb7a0173a3d1a4d7283edbc3723dea3f16892
lamtb26902 <tranbaolam26902@gmail.com>
2022-03-23 00:01:59 +0700

    Add Footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,16 @@ const ToggleSidebar = function() {
 btnSidebar.addEventListener('click', ToggleSidebar);
 sidebarOverlay.addEventListener('click', ToggleSidebar);
 // End: Show/Hide Sidebar
+
+// Start: Footer Information
+var phoneNumber = '+(84)90-2556-655';
+var address = 'Số 01, đường Phù Đổng Thiên Vương, phường 8, thành phố Đà Lạt, tỉnh Lâm Đồng';
+// var email;
+const footerPhoneNumber = document.querySelector('#phone');
+const footerAddress = document.querySelector('#address');
+const footerEmail = document.querySelector('#email');
+footerPhoneNumber.innerHTML = `${phoneNumber}`;
+footerPhoneNumber.href = `tel:${phoneNumber}`;
+footerAddress.innerHTML = `${address}`;
+footerEmail.innerHTML = `Đang cập nhật`
+// End: Footer Information
